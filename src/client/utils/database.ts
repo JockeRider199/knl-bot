e409d47194a15b75bd5d2a2bfdeeb14a605f9715
JockeRider199 <jockerider199@gmail.com>
2023-04-26 22:45:12 +0200

    feat: add database connection

---
+++ src/client/utils/database.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "../../../node_modules/.prisma/client";
+const prisma = new PrismaClient({
+	datasources: {
+		db: {
+			url:
+				process.env.PRODUCTION == "TRUE"
+					? process.env.PROD_DATABASE_URL
+					: process.env.DATABASE_URL,
+		},
+	},
+});
+
+export async function init() {
+	await prisma.$connect().then(() => {
+		console.log("Connected to database");
+	});
+}
