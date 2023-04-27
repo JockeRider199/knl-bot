9737d86f7358e0f8895f1aa0bbcc340b9a1cd36a
JockeRider199 <jockerider199@gmail.com>
2023-04-27 17:28:49 +0200

    feat: temp voice creation

--- src/client/utils/database.ts
+++ src/client/utils/database.ts
@@ -15,3 +15,152 @@ export async function init() {
 		console.log("Connected to database");
 	});
 }
+
+export async function getOrCreateGuild(guildId: string) {
+	let guild = await prisma.guild.findUnique({
+		where: {
+			id: guildId,
+		},
+	});
+
+	if (!guild) {
+		guild = await prisma.guild.create({
+			data: {
+				id: guildId,
+			},
+		});
+	}
+
+	return guild;
+}
+
+export async function isVoiceOwner(
+	guildId: string,
+	channelId: string,
+	ownerId: string
+) {
+	await getOrCreateGuild(guildId);
+
+	const voice = await prisma.voiceChannel.findUnique({
+		where: {
+			channel: {
+				id: channelId,
+				guildId,
+			},
+		},
+		select: {
+			ownerId: true,
+		},
+	});
+
+	if (voice && voice.ownerId == ownerId) {
+		return true;
+	}
+
+	return false;
+}
+
+export async function createVoiceChannel(
+	guildId: string,
+	channelId: string,
+	ownerId: string
+) {
+	await getOrCreateGuild(guildId);
+
+	await prisma.voiceChannel.upsert({
+		where: {
+			channel: {
+				id: channelId,
+				guildId,
+			},
+		},
+		update: {
+			ownerId,
+		},
+		create: {
+			id: channelId,
+			ownerId,
+			guildId,
+		},
+	});
+}
+
+export async function removeVoiceChannel(guildId: string, channelId: string) {
+	await getOrCreateGuild(guildId);
+
+	await prisma.voiceChannel.delete({
+		where: {
+			channel: {
+				id: channelId,
+				guildId,
+			},
+		},
+	});
+}
+
+export async function isTempVoiceChannel(guildId: string, channelId: string) {
+	await getOrCreateGuild(guildId);
+
+	const voice = await prisma.voiceChannel.findUnique({
+		where: {
+			channel: {
+				id: channelId,
+				guildId,
+			},
+		},
+	});
+
+	return voice ? true : false;
+}
+
+export async function setupVoiceChannels(
+	guildId: string,
+	categoryId: string,
+	channelId: string
+) {
+	await getOrCreateGuild(guildId);
+
+	await prisma.guild.update({
+		where: {
+			id: guildId,
+		},
+		data: {
+			voiceCreationCategoryId: categoryId,
+			voiceCreationChannelId: channelId,
+		},
+	});
+}
+
+export async function getVoiceConfig(guildId: string) {
+	await getOrCreateGuild(guildId);
+
+	const guild = await prisma.guild.findUnique({
+		where: {
+			id: guildId,
+		},
+		select: {
+			voiceCreationCategoryId: true,
+			voiceCreationChannelId: true,
+		},
+	});
+
+	return guild;
+}
+
+export async function getVoiceOwnerId(guildId: string, channelId: string) {
+	await getOrCreateGuild(guildId);
+
+	const voice = await prisma.voiceChannel.findUnique({
+		where: {
+			channel: {
+				id: channelId,
+				guildId,
+			},
+		},
+		select: {
+			ownerId: true,
+		},
+	});
+
+	return voice?.ownerId;
+}
